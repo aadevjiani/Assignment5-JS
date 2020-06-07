5199dfad58c92c16818d8d022ee35c01115c3a51
aadevjiani <57323312+aadevjiani@users.noreply.github.com>
2020-06-07 15:44:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+alert("Welcome to Sol@ce - Brings Comfort to Life")
+alert("Error! Please enter a valid password.")
+alert("Welcome to JS Land...\nHappy Coding!")
+alert("Welcome to JS Land...")
+alert("Happy Coding!\nPrevent this page from creating additional dialogs.")

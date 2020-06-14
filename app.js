4a7f9a78b076a4737970c858ab88e30d8f8716c6
aadevjiani <57323312+aadevjiani@users.noreply.github.com>
2020-06-14 18:54:25 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,58 @@
-alert("Welcome to Sol@ce - Brings Comfort to Life")
-alert("Error! Please enter a valid password.")
-alert("Welcome to JS Land...\nHappy Coding!")
-alert("Welcome to JS Land...")
-alert("Happy Coding!\nPrevent this page from creating additional dialogs.")
+// Chapter 1 : Alerts
+// Chapter # 1 - Task # 1
+// alert("Welcome to Sol@ce - Brings Comfort to Life")
+// Chapter # 1 - Task # 2
+// alert("Error! Please enter a valid password.")
+// Chapter # 1 - Task # 3
+// alert("Welcome to JS Land...\nHappy Coding!")
+// Chapter # 1 - Task # 4
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!\nPrevent this page from creating additional dialogs.")
+// Chapter # 1 - Task # 5
+// alert("Hello... I can run JS through my web browser's console")
+
+// Chapter 2 : Variables for Strings
+// Chapter # 2 - Task # 1
+// var username
+// Chapter # 2 - Task # 2
+// var myName = "Ali Abbas Devjiani"
+// Chapter # 2 - Task # 3
+// var message = "Hello World"
+// alert(message)
+// Chapter # 2 - Task # 4
+// var name = "Jhone Doe"
+// var age = "15 years old"
+// var qualification = "Certified Mobile Appliction Development"
+// alert(name)
+// alert(age)
+// alert(qualification)
+// Chapter # 2 - Task # 5
+// var food = " PIZZA \n PIZZ \n PIZ \n PI \n P"
+// alert(food)
+// Chapter # 2 - Task # 6
+// var email ="devjiani@gmail.com"
+// alert("My email address is " + email)
+// Chapter # 2 - Task # 7
+// var book = "A smarter way to learn JavaScript"
+// alert("I am trying to learn from the Book " + book)
+// Chapter # 2 - Task # 8
+// document.write("Yah! I can write HTML conent through JavaScript")
+// Chapter # 2 - Task # 9
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+// Chapter 3 : Variables for Numbers
+// Chapter # 3 - Task # 1
+// var age = 15
+// alert("I am " + age + " years old")
+// Chapter # 3 - Task # 2
+// for(var count = 0; count <= 14; count++)
+// alert("You have visited this site " + count + " times")
+// Chapter # 3 - Task # 3
+// var birthYear = 1978
+// document.write ("My birth year is " + birthYear + "<br\>" + "Data type of my declared variable is number" )
+// Chapter # 3 - Task # 4
+// var visitor = "John Doe"
+// var productTitle = "T-Shirt"
+// var quantity = 5
+// document.write("<b>" + visitor + "</b>" + " ordered " + "<b>" + quantity + "</b>" +" " + "<b>" + productTitle + "</b>"  +"(s) " + "on XYZ Clothing store")
+// Chapter # 3 - Task # 5

40e03b2f84d3a7ea86b23ddadf9f9a7c6e3a3405
aadevjiani <57323312+aadevjiani@users.noreply.github.com>
2020-06-15 23:43:49 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
 // alert("Welcome to JS Land...")
 // alert("Happy Coding!\nPrevent this page from creating additional dialogs.")
 // Chapter # 1 - Task # 5
-// alert("Hello... I can run JS through my web browser's console")
+// console.log("Hello... I can run JS through my web browser's console")
 
 // Chapter 2 : Variables for Strings
 // Chapter # 2 - Task # 1
@@ -97,3 +97,163 @@
 // document.write("Modulus of " + firstNumber + " and " + secondNumber + " is " + modulus)
 
 // Chapter # 5 - Task # 3
+// var number
+// document.write("Value after Variable declaration is : " + number)
+// document.write("<br/><br/>")
+// number = 5
+// document.write("Initial Value : " + number)
+// document.write("<br/><br/>")
+// number = ++number
+// document.write("Value after increment is : " + number)
+// document.write("<br/><br/>")
+// number = number +7
+// document.write("Value after addition is : " + number)
+// document.write("<br/><br/>")
+// number = --number
+// document.write("Value after decrement is : " + number)
+// document.write("<br/><br/>")
+// number = number % 3
+// document.write("The remainder is : " + number)
+// Chapter # 5 - Task # 4
+// var ticket = 600
+// var quantity = 5
+// var cost = ticket * quantity
+// document.write("Total cost to buy " + quantity + " tickets to a movie is " + cost + "PKR")
+// Chapter # 5 - Task # 5
+// var table = prompt("Enter the number to generate table : ")
+// document.write("<h2> Table of : " + table + "</h2>")
+// for (i=1; i<=10; i++)
+// document.write(table + " * " + i + " = " + table*i + "<br/>")
+// Chapter # 5 - Task # 6
+// var tempC = 25
+// var tempF = 70
+// var tempCtoF = ((tempC*9/5)+32)
+// var tempFtoC = ((tempF-32)*5/9)
+// document.write(tempC + " <sup>o</sup> C " + "is " + tempCtoF + " <sup>o</sup> F")
+// document.write("<br/><br/>")
+// document.write(tempF + " <sup>o</sup> F " + "is " + tempFtoC + " <sup>o</sup> C")
+// Chapter # 5 - Task # 7
+// var priceOfItem1 = 650
+// var priceOfItem2 = 100
+// var qtyOfItem1 = 3
+// var qtyOfItem2 = 7
+// var shippingCharges = 100
+// var totalCost = ((priceOfItem1*qtyOfItem1)+(priceOfItem2*qtyOfItem2)+shippingCharges)
+// document.write("<h2>Shopping Cart</h2>")
+// document.write("Pricce of Item 1 is " + priceOfItem1 + "<br/>")
+// document.write("Quantity of Item 1 is " + qtyOfItem1 + "<br/>")
+// document.write("Pricce of Item 2 is " + priceOfItem2 + "<br/>")
+// document.write("Quantity of Item 2 is " + qtyOfItem2 + "<br/>")
+// document.write("Shipping Charges " + shippingCharges + "<br/")
+// document.write("<br/><br/>")
+// document.write("Total Cost of your Order is " + totalCost)
+// Chapter # 5 - Task # 8
+// var marksInSub1 = 350
+// var marksInSub2 = 454
+// var totalMarksObt = marksInSub1 + marksInSub2
+// var totalMarks = 980
+// var per = ((totalMarksObt/totalMarks)*100)
+// document.write("<h2>Marks Sheet</h2>")
+// document.write("<br/><br/>")
+// document.write("Total Marks : " + totalMarks + "<br/>")
+// document.write("Marks Obtained : " + totalMarksObt + "<br/>")
+// document.write("Percentage : " + per + "%")
+// Chapter # 5 - Task # 9
+// var USD = 10
+// var SR = 25
+// var PKR = (USD*104.80) + (SR*28)
+// document.write("<h2>Currency in PKR</h2>")
+// document.write("<br/><br/>")
+// document.write("Total Currency in PKR : " + PKR)
+// Chapter # 5 - Task # 10
+// var initNum = 5
+// result = (initNum + 5) * 10 / 2 
+// console.log(result)
+// Chapter # 5 - Task # 11
+// var currentYear = 2016
+// var yearOfBirth = 1992
+// var age = currentYear - yearOfBirth
+// document.write("<h2>Age Calculator</h2>")
+// document.write("<br/><br/>")
+// document.write("Current Year : " + currentYear + "<br/>")
+// document.write("Birth Year : " + yearOfBirth + "<br/>")
+// document.write("Your Age is : " + age)
+// Chapter # 5 - Task # 12
+// var radius = 20
+// var circum = 2*3.142*radius
+// var area = 3.142*radius*2
+// document.write("<h2>The Geometrizer</h2>")
+// document.write("<br/><br/>")
+// document.write("Radius of a Circle : " + radius + "<br/>")
+// document.write("The circumference is : " + circum + "<br/>")
+// document.write("The area is : " + area)
+// Chapter # 5 - Task # 13
+// var favSnack = "Chocolate Chip"
+// var currAge = 15
+// var maxAge = 65
+// var qtySnackPerDay = 3
+// var reqd = ((maxAge-currAge)*365*qtySnackPerDay)
+// document.write("<h2>The Lifetime Supply Calculator</h2>")
+// document.write("<br/><br/>")
+// document.write("Favourite Snack : " + favSnack + "<br/>")
+// document.write("Current age : " + currAge + "<br/>")
+// document.write("Estimated Maximum Age :" + maxAge + "<br/>")
+// document.write("Amount of Snacks per Day : " + qtySnackPerDay + "<br/>")
+// document.write("You will need " + reqd +" " + favSnack + " to last you untill the ripe old age of " + maxAge)
+
+// Chapter 6-9 : Math Expressions
+// Chapter # 6-9 - Task # 1
+// var a = 10
+// document.write("Result : <br/>" )
+// document.write("The value of a is : " + a + "<br/>")
+// document.write("====================================== <br/><br/>")
+// document.write("The value of ++a is : " + ++a + "<br/>")
+// document.write("Now the value of a is : " + a + "<br/><br/>")
+// document.write("The value of a++ is : " + a++ + "<br/>")
+// document.write("Now the value of a is : " + a + "<br/><br/>")
+// document.write("The value of --a is : " + --a + "<br/>")
+// document.write("Now the value of a is : " + a + "<br/><br/>")
+// document.write("The value of a-- is : " + a-- + "<br/>")
+// document.write("Now the value of a is : " + a + "<br/><br/>")
+// Chapter # 6-9 - Task # 2
+// var a = 2
+// var b = 1
+// var result = --a - --b + ++b +b--
+// document.write("a is " + a + "<br/>")
+// document.write("b is " + b + "<br/>")
+// document.write("result is " + result)
+// Chapter # 6-9 - Task # 3
+// var name = prompt("Enter your Name : ")
+// document.write("Mr/Ms <b>" + name + "</b> Welcome to the Java World")
+// Chapter # 6-9 - Task # 4
+// BLANK
+// Chapter # 6-9 - Task # 5
+// var table = prompt("Enter the Number of Table to generate : ","5")
+//     for(i=1; i<=10; i++)
+//     document.write(table + " * " + i + " = " + table*i + "<br/>")
+// Chapter # 6-9 - Task # 6
+// var sub1 = "English"
+// var sub2 = "Maths"
+// var sub3 = "Urdu"
+// var totalMarks = 100
+// var marksObtSub1 = +prompt("Enter Marks Obtained in " + sub1 )
+// var marksObtSub2 = +prompt("Enter Marks Obtained in " + sub2 )
+// var marksObtSub3 = +prompt("Enter Marks Obtained in " + sub3 )
+// var totalMarksObt = (marksObtSub1 + marksObtSub2 + marksObtSub3)
+// var perSub1 = (marksObtSub1/totalMarks*100)
+// var perSub2 = (marksObtSub2/totalMarks*100)
+// var perSub3 = (marksObtSub3/totalMarks*100)
+// var totalPer = (totalMarksObt/(totalMarks*3)*100)
+// document.write(
+//     "<table>"+
+//     "<tr><th>Subject</th><th> Total Marks</th><th> Obtained Marks </th> <th> Percentage </th> </tr>"+
+//     "<tr><td>"+sub1+"</td><td><center>"+totalMarks+"</center></td><td><center>"+marksObtSub1+"</center></td><td><center>"+perSub1+"%"+"</center></td></tr>"+
+//     "<tr><td>"+sub2+"</td><td><center>"+totalMarks+"</center></td><td><center>"+marksObtSub2+"</center></td><td><center>"+perSub2+"%"+"</center></td></tr>"+
+//     "<tr><td>"+sub3+"</td><td><center>"+totalMarks+"</center></td><td><center>"+marksObtSub3+"</center></td><td><center>"+perSub3+"%"+"</center></td></tr>"+
+//     "<tr><td></td><td><center><b>"+totalMarks*3+"</b></center></td><td><center><b>"+totalMarksObt+"</b></center></td><td><center><b>"+totalPer+"%"+"</b></center></td></b></tr>"+
+//     "</table>"
+// )
+
+// Chapter 9-11 : User Input & Conditional Statement
+// Chapter # 9-11 - Task # 1
+
